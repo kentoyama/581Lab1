57b134e166c56783abc4fab5b7d8a47d743f8d7d
kentoyama <46545165+kentoyama@users.noreply.github.com>
2020-02-16 15:43:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
 # Write your program here
-motor_right = Motor(Port.B, Direction.COUNTERCLOCKWISE)
-motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE)
-robot = DriveBase(motor_left, motor_right, 56, 130)
-robot.drive_time(300, 0, 4000)
+motor_right = Motor(Port.B, Direction.COUNTERCLOCKWISE) #initialize right motor
+motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE) #initialize left motor
+robot = DriveBase(motor_left, motor_right, 56, 130) #connects both motors to singular robot
+robot.drive_time(300, 0, 4000) #300 mm/s for 4000 ms  = 1200 mm travelled
 brick.sound.beep()
+
+while not Button.CENTER in brick.buttons():
+    wait(10)
 
 ultrasonicSensor = UltrasonicSensor(port)
 while ultrasonicSensor.distance() > 500: # + distance the sensor is from the front of the robot
-    robot.drive_time(300, 0, 2)
+    robot.drive(300, 0)
+    wait(5)
 brick.sound.beep()
 while not button.CENTER in brick.buttons():
     wait(10)
