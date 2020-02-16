73a1f2c6d23b6b50a22d17cf62851f5f46b15213
kentoyama <46545165+kentoyama@users.noreply.github.com>
2020-02-16 16:25:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,31 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
 # Write your program here
-motor_right = Motor(Port.B, Direction.COUNTERCLOCKWISE) #initialize right motor
-motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE) #initialize left motor
-robot = DriveBase(motor_left, motor_right, 56, 130) #connects both motors to singular robot
-robot.drive_time(300, 0, 4000) #300 mm/s for 4000 ms  = 1200 mm travelled
+motor_right = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+motor_left = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+robot = DriveBase(motor_left, motor_right, 56, 130)
+robot.drive_time(300, 0, 3900)
+wait(1000)
 brick.sound.beep()
-
 while not Button.CENTER in brick.buttons():
     wait(10)
 
-ultrasonicSensor = UltrasonicSensor(port)
-while ultrasonicSensor.distance() > 500: # + distance the sensor is from the front of the robot
-    robot.drive_time(300, 0, 2)
+
+ultrasonicSensor = UltrasonicSensor(Port.S1)
+
+while ultrasonicSensor.distance() > 530 or ultrasonicSensor.distance() < 470: # + distance the sensor is from the front of the robot
+    if ultrasonicSensor.distance() > 510:
+        robot.drive(150,0)
+    if ultrasonicSensor.distance() < 490:
+        robot.drive(-150, 0)
+wait(1500)
+while ultrasonicSensor.distance() > 505 or ultrasonicSensor.distance() < 495: # + distance the sensor is from the front of the robot
+    if ultrasonicSensor.distance() > 505:
+        robot.drive(50,0)
+    if ultrasonicSensor.distance() < 495:
+        robot.drive(-50, 0)
+wait(500)
 brick.sound.beep()
-while not button.CENTER in brick.buttons():
+while not button.CENTER in brick.Buttons():
     wait(10)
+
